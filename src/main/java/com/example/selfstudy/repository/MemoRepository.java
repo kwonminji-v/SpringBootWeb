b58d361d92d62511ad33104c6311dadf5ad117a8
kwonminji <minji6313@yahoo.com>
2024-03-02 19:04:34 +0900

    "#4. Paging & @Query"

--- src/main/java/com/example/selfstudy/repository/MemoRepository.java
+++ src/main/java/com/example/selfstudy/repository/MemoRepository.java
@@ -1,8 +1,23 @@
 package com.example.selfstudy.repository;
 
 import com.example.selfstudy.entity.Memo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+
+    List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);
+
+    Page<Memo> findByMnoBetween(Long from, Long to, Pageable pageable);
+
+    void deleteMemosByMnoLessThan(Long num);
+
+    @Query("select m from Memo m order by m.mno desc")
+    List<Memo> getListdesc();
+
 
 }
--- src/test/java/com/example/selfstudy/SelfStudyApplicationTests.java
+++ src/test/java/com/example/selfstudy/SelfStudyApplicationTests.java
@@ -8,8 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -20,6 +23,56 @@ class SelfStudyApplicationTests {
     @Autowired
     MemoRepository memoRepository;
 
+    @Test
+    public void getselectQuery() {
+
+
+        List<Memo> memo = memoRepository.getListdesc();
+
+        System.out.println(memo);
+
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDeleteQueryMethods() {
+        memoRepository.deleteMemosByMnoLessThan(10L );
+    }
+
+    @Test
+    public void testQueryMethodWithPageable() {
+
+        Pageable pageable = PageRequest.of(0,10,Sort.by("mno").descending());
+
+        Page<Memo> result = memoRepository.findByMnoBetween(10L,50L,pageable);
+        result.get().forEach(memo -> System.out.println(memo));
+    }
+
+    @Test
+    public void testQueryMethods() {
+
+        List<Memo> list = memoRepository.findByMnoBetweenOrderByMnoDesc(70L, 80L);
+
+        for (Memo memo : list) {
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testSort() {
+
+        Sort sort1 = Sort.by("mno").descending();
+
+        Pageable pageable = PageRequest.of(0, 10, sort1);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        result.forEach(memo -> {
+            System.out.println(memo);
+        });
+    }
+
     //1페이지 10개씩
     @Test
     public void testPageDefault() {
@@ -29,9 +82,23 @@ class SelfStudyApplicationTests {
         Page<Memo> result = memoRepository.findAll(pageable);
 
         System.out.println(result);
+        System.out.println();
         System.out.println("========================================================");
-        System.out.println("Total page(총 몇개) = " + result.getTotalPages());
-        System.out.println("Total Count");
+        System.out.println("Total page(총 몇 페이지) = " + result.getTotalPages());
+        System.out.println("Total Count(총 몇개) = " + result.getTotalElements());
+        System.out.println("Page Number (현재 페이지 번호) = " + result.getNumber());
+        System.out.println("Page Size (페이지당 데이터 갯수) = " + result.getSize());
+        System.out.println("Has Next Page (다음페이지 존재 여부)" + result.hasNext());
+        System.out.println("First page (시작페이지 0 여부) = " + result.isFirst());
+        System.out.println();
+        System.out.println("============================");
+
+        System.out.println();
+
+        for (Memo memo : result.getContent()) {
+            System.out.println(memo);
+        }
+
     }
 
     @Test
@@ -83,12 +150,12 @@ class SelfStudyApplicationTests {
         System.out.println(memoRepository.save(memo));
     }
 
-//    @Test
-//    public void testInsertFakeData() {
-//        IntStream.rangeClosed(1,50).forEach(i -> {
-//            Memo memo = Memo.builder().memoText("샘플 데이터 ..." + i).build();
-//            memoRepository.save(memo);
-//        });
-//    }
+    @Test
+    public void testInsertFakeData() {
+        IntStream.rangeClosed(1,100).forEach(i -> {
+            Memo memo = Memo.builder().memoText("샘플 데이터..." + i).build();
+            memoRepository.save(memo);
+        });
+    }
 
 }
